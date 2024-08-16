5bddbc75897d9168f270e7e82277a8c57baef7c0
otabekoff <sadiridinovotabek@gmail.com>
2024-08-16 09:43:58 +0500

    Workflow set for Pages

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/calendara/",
   title: "Calendara",
   description: "A customizable and versatile calendar library for dynamic date handling.",
   locales: {
